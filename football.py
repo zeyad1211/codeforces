414212eca16fa7ef400d7ee151428c22f9753846
zeyad <zeyadwagih@gmail.com>
2024-09-06 17:41:23 +0400

    first

---
+++ football.py
@@ -0,0 +1,13 @@
+n = int(input())
+diction = {}
+for i in range(n):
+    inpt = input()
+    if inpt not in diction:
+        diction[inpt] = 0
+    diction[inpt] +=1
+
+val = max(diction.values())
+for key,value in diction:
+    if value == val:
+        print(key)
+        break
