64e70926644de6cfca77c5548d64bdbbbf66b5db
zeyad <zeyadwagih@gmail.com>
2024-09-06 19:28:40 +0400

    some addjustments

--- Brains.py
+++ Brains.py
@@ -1,8 +1,13 @@
 n, m = map(int, input().split())
 lst = []
+color = 0
 for i in range(n):
     lst.append(list(input().split()))
-if 'C' or 'M' or 'Y' in lst:
+for arr in lst:
+    if ('C' in arr or 'M' in arr or 'Y' in arr):
+        color = 1
+        break
+if (color == 1):
     print("#Color")
 else:
-    print("#Black&White")
+    print("Black&White")
