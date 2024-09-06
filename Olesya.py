008e87db9d61c82c300587b79b04ce2927bfd9a2
zeyad <zeyadwagih@gmail.com>
2024-09-06 17:44:43 +0400

    added a comment

--- Olesya.py
+++ Olesya.py
@@ -2,7 +2,7 @@ n, t = map(int, input().split())
 smallestNumber = 10**(n-1)
 largestNumber = 10**(n)
 ans = -1
-for i in range(smallestNumber, largestNumber):
+for i in range(smallestNumber, largestNumber): #largest Number is not included
     if (i%t == 0):
         ans = i
         break
