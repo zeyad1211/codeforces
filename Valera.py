4f5eb451409cfcb6eb9c231c532042928dc1dfdf
zeyad <zeyadwagih@gmail.com>
2024-09-09 00:18:38 +0400

    fixed minor bugs

--- Valera.py
+++ Valera.py
@@ -9,22 +9,22 @@ def isX():
     for j in range(0, n):
         for k in range(0, round((n/2))):
             if (lst[j][k] == lst[j][-k-1] and lst[j][k] == lst[-j-1][k]):
-                string = "YES"
+                # string = "YES"
                 if j == k or j+k == n-1:
                     if lst[j][k] != lst[0][0]:
                         string = "NO"
-                        print(1)
+                        # print(1)
                         return string
                 else:
                     if lst[j][k] != lst[0][1]:
                         string = "NO"
-                        print(2)
+                        # print(2)
                         return string
             else:
                 string = "NO"
-                print(3)
+                # print(3)
                 return string
-                
+    return string
         # print(lst)
     print(string)
 answer = isX()
