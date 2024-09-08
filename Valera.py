460784fbb3797b220096d471affb6499c9ae6150
zeyad <zeyadwagih@gmail.com>
2024-09-08 23:53:23 +0400

    Hoping it works

--- Valera.py
+++ Valera.py
@@ -1,16 +1,31 @@
-n = int(input())
-string = "YES"
-lst = [""]*(n)
-for i in range(0,n):
-    lst[i] = input()
-temp = 0
-temp2 = n-1
-for j in range(0, n):
-    for k in range(0, round((n)/2)):
-        if (lst[j][k] != lst[j][-k-1]):
-            string = "NO"
-            # print(f"{lst[j][k]}, {lst[j][-k-1]}")
-            # print(f"{j}, {k}")
-            break
-# print(lst)
-print(string)
+def isX():
+    n = int(input())
+    string = "YES"
+    lst = [""]*(n)
+    for i in range(0,n):
+        lst[i] = input()
+    temp = 0
+    temp2 = n-1
+    for j in range(0, round(n/2)):
+        for k in range(0, round((n/2))):
+            if (lst[j][k] == lst[j][-k-1] and lst[j][k] == lst[-j-1][k]):
+                string = "YES"
+                if j == k:
+                    if lst[j][k] != lst[0][0]:
+                        string = "NO"
+                        print(1)
+                        return string
+                else:
+                    if lst[j][k] != lst[0][1]:
+                        string = "NO"
+                        print(2)
+                        return string
+            else:
+                string = "NO"
+                print(3)
+                return string
+                
+        # print(lst)
+    print(string)
+answer = isX()
+print(answer)
