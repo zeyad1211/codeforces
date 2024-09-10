ef95ba0401c3284fa01fe116ca18068ba41ab448
zeyad <zeyadwagih@gmail.com>
2024-09-10 17:06:14 +0400

    it worked somehow!

--- Arpa.py
+++ Arpa.py
@@ -1,6 +1,16 @@
 n = int(input())
-ans = 1
-for i in range(n):
-    ans *= 8
-    ans %= 10
-print(ans)
+# ans = 1
+# for i in range(n):
+#     ans *= 8
+#     ans %= 10
+# print(ans)
+if n == 0:
+    print(1)
+elif n in range(1, 10**9+1, 4):
+    print(8)
+elif n in range(2, 10**9+1, 4):
+    print(4)
+elif n in range(3, 10**9+1, 4):
+    print(2)
+else:
+    print(6)
