2f5492d1387adfe5d92d2aae07a3642d3298ca86
zeyad <zeyadwagih@gmail.com>
2024-09-11 11:28:44 +0400

    second Try

--- counterExample.py
+++ counterExample.py
@@ -1,22 +1,15 @@
 def run():
     l, r = map(int, input().split())
-    cf =  []
-    for i in range(2, l+1):
-        if l % i == 0 and r % i == 0:
-            commonFactor = i
-            b = l + commonFactor - 1
-            if b < r:
-                print(l, b, r)
-                return
-            else:
-                print(-1)
-                return
-        if l%i == 0:
-            cf.append(i)
-    c = l*cf[0]
-    if c < r:
-        print(l, l+1,l*cf[0])
-    else:
+    if r - l <= 1:
         print(-1)
+        return
+    if l % 2 != 0:
+        l += 1
+    if r % 2 != 0:
+        r -= 1
+    if r - l <= 1:
+        print(-1)
+        return
+    print(l, l+1, l+2) 
     return
 run()
