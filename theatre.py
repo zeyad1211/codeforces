6b3a2f5c5a6dfcf37b317a9af387d64a8c00cfcc
zeyad <zeyadwagih@gmail.com>
2024-09-11 11:04:26 +0400

    First

---
+++ theatre.py
@@ -0,0 +1,12 @@
+import math
+n, m, a = map(int, input().split())
+maxi = max(n, m)
+if n > a and m > a:
+    rows = math.ceil(n/a)
+    cols = math.ceil(m/a)
+    ans = rows + cols
+elif maxi > a:
+    ans = math.ceil(maxi/a)
+else:
+    ans = 1
+print(ans)
