f9eb4a384a0b379645c72afb52f9a02bf1396079
zeyad <zeyadwagih@gmail.com>
2024-09-19 23:49:44 +0400

    silly mistakes

--- IQTest.py
+++ IQTest.py
@@ -2,14 +2,14 @@ def run():
     lst = [""]*4
     start1 = 0
     start2 = 0
-    hashcount = 0
-    dotcount = 0
     stop1 = 2
     stop2 = 2
     for p in range(4):
         lst[p] = input()
     # print(lst)
     while(stop1 < 5):
+        hashcount = 0
+        dotcount = 0
         for row in range(start1, stop1):
             for col in range(start2, stop2):
                 if lst[row][col] == '#':
@@ -20,7 +20,7 @@ def run():
             return "YES"
         start2 += 1
         stop2 += 1
-        if stop2 == 4:
+        if stop2 == 5:
             start2 = 0
             stop2 = 2
             start1 += 1
